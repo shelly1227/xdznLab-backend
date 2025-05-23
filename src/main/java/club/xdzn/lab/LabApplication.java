5dd75063766e2ec7b66679482f57c5a866b5c349
shelly1227 <40505282@qq.com>
2025-05-23 15:04:06 +0800

    feat: initialize project structure, install dependencies, and configure base files

---
+++ src/main/java/club/xdzn/lab/LabApplication.java
@@ -0,0 +1,11 @@
+package club.xdzn.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LabApplication.class, args);
+    }
+}
